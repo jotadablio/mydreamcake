b6b2439fe5972c6468f1e6ce42994abc917d0f1d
jotadablio <jwilly1409@gmail.com>
2024-12-02 00:56:13 -0300

    "Added StatusBar component to App.js, removed StatusBar from AddFundsScreen, and updated styles in AddFunds and Shop components."

--- app/Login/index.js
+++ app/Login/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Switch, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../context/AuthContext';
 import styles from './styles';
@@ -17,15 +17,45 @@ export default function LoginScreen({ navigation }) {
     }
   };
   
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Olá! Para continuar, faça o seu login</Text>
-        <TextInput placeholder="E-mail" style={styles.input} />
-        <TextInput placeholder="Senha" secureTextEntry style={styles.input} />
-        <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Entrar</Text>
+     return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Olá! Para continuar, faça o seu login</Text>
+
+      {/* Campos de entrada */}
+      <TextInput placeholder="E-mail" style={styles.input} />
+      <TextInput placeholder="Senha" secureTextEntry style={styles.input} />
+
+      {/* Opções */}
+      <View style={styles.options}>
+        <View style={styles.option}>
+          <Switch value={false} />
+          <Text style={styles.optionText}>Continuar logado</Text>
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.optionText}>Esqueci minha senha</Text>
         </TouchableOpacity>
       </View>
-    );
-  }
 
+      {/* Botão de login */}
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+
+      {/* Login social */}
+      <Text style={styles.orText}>ou entre com:</Text>
+      <View style={styles.socialButtons}>
+        <TouchableOpacity style={styles.socialButton}>
+          <Image source={require('../../assets/icons/google.png')} style={styles.socialIcon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.socialButton}>
+          <Image source={require('../../assets/icons/facebook.png')} style={styles.socialIcon} />
+        </TouchableOpacity>
+      </View>
+
+      {/* Registro */}
+      <TouchableOpacity>
+        <Text style={styles.registerText}>Ainda não possui cadastro?</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
